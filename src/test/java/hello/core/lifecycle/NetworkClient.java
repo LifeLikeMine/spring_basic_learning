11cc756b9da93e573e4a24c81dab96a8b26adfbf
bskim <skdkwl900@gmail.com>
2023-08-30 16:42:33 +0900

    2023 8/30 learning Spring

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,15 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 public class NetworkClient {
     private String url;
 
     public NetworkClient(){
-        System.out.println("url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+        System.out.println("생성자 호출, url = " + url);
     }
 
     public void setUrl(String url) {
@@ -23,5 +26,18 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close : "+url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
